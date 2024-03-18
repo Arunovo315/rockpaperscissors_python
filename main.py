286d8c009e8c291d446a791807648772858f55aa
Arunovo Ferdouse <118589868+Arunovo315@users.noreply.github.com>
2024-03-19 04:59:52 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,48 @@
 import random
 
 print("Rock, Paper, Scissors game")
+points = 0
+attempts = 0
+print("Your points: ", points)
+print("To quit, type 'q' ")
 
 choices = ["rock", "paper", "scissors"]
 
-p1 = input("Enter your choice : ")
-bot = random.choice(choices)
 
-if p1 == choices[0] and bot == choices[1]:
-  print("Computer wins")
-elif p1 == choices[1] and bot == choices[2]:
-  print("Computer wins")
-elif p1 == choices[2] and bot == choices[0]:
-  print("Computer wins")
-elif p1 == choices[2] and bot == choices[1]:
-  print("You won!")
-elif p1 == choices[1] and bot == choices[0]:
-  print("You won!")
-elif p1 == choices[1] and bot == choices[0]:
-  print("You won!")
-elif p1 == bot:
-  print("Draw")
-else:
-  print("Invalid input")
+while True:
 
-print("Computers choice : " + bot)
+  p1 = input("Enter your choice : ")
+  bot = random.choice(choices)
+  print("Computers choice : " + bot)
+  
+  if p1 == choices[0] and bot == choices[1]:
+    print("Computer wins")
+    attempts += 1
+  elif p1 == choices[1] and bot == choices[2]:
+    print("Computer wins")
+    attempts += 1
+  elif p1 == choices[2] and bot == choices[0]:
+    print("Computer wins")
+    attempts += 1
+  elif p1 == choices[2] and bot == choices[1]:
+    points += 1
+    print("You won! Total points = ", points)
+    attempts += 1
+  elif p1 == choices[1] and bot == choices[0]:
+    points += 1
+    print("You won! Total points = ", points)
+    attempts += 1
+  elif p1 == choices[0] and bot == choices[2]:
+    points +=1
+    print("You won! Total points = ", points)
+    attempts += 1
+  elif p1 == bot:
+    print("Draw")
+    attempts += 1
+  elif p1 == "q":
+    print("Thanks for playing..... Your points: ", points, ", Your attempts: ", attempts)
+    attempts += 1
+    break
+  else:
+    print("Invalid input")
+    attempts += 1
